cc0e2525e98047a0d36a3fe82b4bfe1e2adedc68
epq <aerial.exe@gmail.com>
2020-01-10 23:21:24 -0500

    Update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,11 +34,9 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Yuannada building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
               </a>
             </p>
           </Container>
